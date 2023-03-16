850770e3772e5c51850fda95658f15e9fb09569d
ChelseaKubasu2 <kubasuchelsea@gmail.com>
2023-03-16 19:42:00 +0000

    add files

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python3
+""" A method that checks if a given data set represents a valid UTF-8 """
+
+
+def EightBits(integer):
+    """ A function that:
+    converts an integer to its binary representation,
+#!/usr/bin/python3
+""" A method that checks if a given data set represents a valid UTF-8 """
+
+
+def EightBits(integer):
+    """ A function that:
+    converts an integer to its binary representation,
+    removes the 'Ob' prefixe
+    and returns the full 8 bits of the binary string """
+    binary = bin(integer)[2:]
+    add = '0' * (8 - len(binary))
+    return add + binary
 
 
 def validUTF8(data):
-    """ Determine if data set represents a valid UTF-8 encoding  """
-    bytes = 0
-    for bit in data:
-        mask = 1 << 7
-        if bytes == 0:
-            while mask & bit:
-                bytes += 1
-                mask >>= 1
-            if bytes == 0:
-                continue
-            if bytes == 1 or bytes > 4:
-                return False
+    """ A method that checks if a given data set represents a valid UTF-8 """
+    i = 0
+    while i < len(data):
+        if EightBits(data[i])[0] == '0':
+            i += 1
+        elif (EightBits(data[i])[:3] == '110' and i + 1 < len(data) and
+                EightBits(data[i + 1])[:2] == '10'):
+            i += 2
+        elif (EightBits(data[i])[:4] == '1110' and i + 2 < len(data) and
+                EightBits(data[i + 1])[:2] == '10' and
+                EightBits(data[i + 1])[:2] == '10'):
+            i += 3
+        elif (EightBits(data[i])[:5] == '11110' and i + 3 < len(data) and
+                EightBits(data[i + 1])[:2] == '10' and
+                EightBits(data[i + 2])[:2] == '10' and
+                EightBits(data[i + 3])[:2] == '10'):
+            i += 4
         else:
-            if bit >> 6 != 2:
-                return False
-        bytes -= 1
-    return bytes == 0
+            return False
+    return True
