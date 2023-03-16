888c2f6f9a82499672a6c5b32de75bc8bd54fe53
ChelseaKubasu2 <kubasuchelsea@gmail.com>
2023-03-16 12:32:19 -0700

    add files

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,29 @@
 #!/usr/bin/node
 const request = require('request');
-let characters;
-const dict = {};
-request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-  if (res.statusCode === 200) {
-    characters = JSON.parse(body).characters;
-    for (const url of characters) {
-      request.get(url, function (err1, res1, body1) {
-        if (res1.statusCode === 200) {
-          savedData(url, JSON.parse(body1).name);
-        } else if (err1) {
-          console.log(err1);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const data = [];
+request(url, function (err, res, body) {
+  if (err) console.log(err);
+  else if (res.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    for (let i = 0; i < characters.length; i++) {
+      data.push({ url: characters[i], name: '' });
+      request(characters[i], function (error, resp, body2) {
+        if (error) console.log(error);
+        data[i].name = JSON.parse(body2).name;
+        if (checkdata(data)) {
+          for (let j = 0; j < data.length; j++) {
+            console.log(data[j].name);
+          }
         }
+        // console.log(JSON.parse(body2).name);
       });
     }
-  } else if (err) {
-    console.log(err);
   }
 });
-function savedData (url, name) {
-  dict[url] = name;
-  if (Object.entries(dict).length === characters.length) {
-    for (const url of characters) {
-      console.log(dict[url]);
-    }
+function checkdata (data) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name === '') { return false; }
   }
+  return true;
 }
